0d3d57520699f03b1656f2f64f93c26221b92998
SmartLabradorTech <smarthuskytech@hotmail.com>
2016-11-04 16:48:08 -0700

    largest number

--- src/number/LargestNumber/LargestNumber.java
+++ src/number/LargestNumber/LargestNumber.java
@@ -1,12 +1,63 @@
 package number.LargestNumber;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by tianleh on 10/24/16.
  */
+
+class SpeComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        String first = o1.toString();
+        String second = o2.toString();
+
+        for (int i = 0; ; i++) {
+            if (i >= first.length()) {
+                if (i >= second.length()) {
+                    return 0;
+                }
+
+                return 1;
+            }
+
+            if (i >= second.length()) {
+                return -1;
+            }
+
+            if (first.charAt(i) > second.charAt(i)) {
+                return 1;
+            }
+
+            if (first.charAt(i) < second.charAt(i)) {
+                return -1;
+            }
+
+        }
+    }
+}
+
 public class LargestNumber {
 
     public String largestNumber(int[] nums) {
 
-        return null;
+        int length = nums.length;
+
+        //Arrays.sort(nums, new SpeComparator());
+
+        String result = "";
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {3, 30, 34, 5, 9};
+
+        LargestNumber largestNumber = new LargestNumber();
+
+        System.out.println(largestNumber.largestNumber(input));
     }
 }
+
--- src/play/StringsRelated.java
+++ src/play/StringsRelated.java
@@ -1,5 +1,9 @@
 package play;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by tianleh on 10/18/16.
  */
@@ -7,7 +11,27 @@ public class StringsRelated {
 
     public static void main(String[] args) {
 
-        String a = "abc";
-        
+        String a = "0";
+        String b = "11";
+        String c = "1";
+
+        List<String> strings = Arrays.asList(a, b, c);
+
+        String[] objects = (String[]) strings.toArray();
+
+        print(objects);
+
+        Arrays.sort(objects);
+
+        print(objects);
+    }
+
+    public static void print(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
+
+        System.out.println();
+
     }
 }

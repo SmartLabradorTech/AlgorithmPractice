1583fa148b5b2078cc5867767e727e0cb1be361f
SmartLabradorTech <tianlehuang@gmail.com>
2017-10-11 00:06:19 -0700

    Time exceed RangeSumQuery

---
+++ src/array/CountofSmallerNumbersAfterSelf/CountofSmallerNumbersAfterSelf.java
@@ -0,0 +1,14 @@
+package array.CountofSmallerNumbersAfterSelf;
+
+import java.util.List;
+
+/**
+ * Created by tianle on 10/10/17.
+ */
+public class CountofSmallerNumbersAfterSelf {
+
+    public List<Integer> countSmaller(int[] nums) {
+
+        return null;
+    }
+}
---
+++ src/array/RangeSumQuery/RangeSumQuery.java
@@ -0,0 +1,45 @@
+package array.RangeSumQuery;
+
+/**
+ * Created by tianle on 10/10/17.
+ * https://leetcode.com/problems/range-sum-query-mutable/description/
+ */
+public class RangeSumQuery {
+
+    private int[] nums;
+    private int[][] sums;
+
+    public RangeSumQuery(int[] nums) {
+        this.nums = nums;
+
+        int length = nums.length;
+
+        sums = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+
+            int sum = 0;
+
+            for (int j = i; j < length; j++) {
+
+                sum += nums[j];
+                sums[i][j] = sum;
+            }
+        }
+    }
+
+    public void update(int i, int val) {
+        for (int p = 0; p <= i; p++) {
+            for (int q = i; q < nums.length; q++) {
+                sums[p][q] -= nums[i];
+                sums[p][q] += val;
+            }
+        }
+
+        nums[i] = val;
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[i][j];
+    }
+}

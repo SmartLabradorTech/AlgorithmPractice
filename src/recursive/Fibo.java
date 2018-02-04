1afc1dbdefc13d9475ddbdb928508e3976289909
SmartLabradorTech <tianlehuang@gmail.com>
2018-02-03 23:47:50 -0800

    fibo array

--- src/recursive/Fibo.java
+++ src/recursive/Fibo.java
@@ -1,5 +1,9 @@
 package recursive;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * Created by tianle on 2/3/18.
  */
@@ -14,11 +18,35 @@ public class Fibo {
         return f(n - 1) + f(n - 2);
     }
 
+    public int fDP(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int left = 1;
+        int right = 1;
+
+        int current = 0;
+
+        for (int i = 3; i <= n; i++) {
+            current = left + right;
+
+            left = right;
+            right = current;
+        }
+
+        return current;
+    }
+
     public static void main(String[] args) {
         Fibo fibo = new Fibo();
 
         int n = 1000;
 
-        System.out.println(fibo.f(n));
+        Set<String> a = new HashSet<>();
+
+        PriorityQueue<Integer> b = new PriorityQueue<>();
+
+        System.out.println(fibo.fDP(n));
     }
 }

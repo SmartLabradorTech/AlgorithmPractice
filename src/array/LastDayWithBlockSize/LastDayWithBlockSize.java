5d53d1fb16b1c7fccdcda7dd16f17c83e5827680
SmartLabradorTech <tianlehuang@gmail.com>
2018-02-24 01:19:13 -0800

    brute force to solve LastDayWithBlockSize

---
+++ src/array/LastDayWithBlockSize/LastDayWithBlockSize.java
@@ -0,0 +1,68 @@
+package array.LastDayWithBlockSize;
+
+import java.util.Arrays;
+
+/**
+ * Created by tianle on 2/23/18.
+ */
+public class LastDayWithBlockSize {
+
+    /**
+     * operates on virtual array value[]
+     *
+     * @param schedule permutation of 1 to N, indicates on day i, value[schedule[i]] is 1
+     * @param k
+     * @return
+     */
+    public int lastDay(int[] schedule, int k) {
+
+        int length = schedule.length;
+        int[] value = new int[length + 1];
+
+        int lastDay = -1;
+
+        for (int i = 0; i < length; i++) {
+
+            value[schedule[i]] = 1;
+            boolean hasSize = hasSize(value, length, k);
+
+            System.out.println(Arrays.toString(value) + ", result is " + hasSize);
+
+            if (hasSize) {
+                lastDay = Math.max(lastDay, i);
+            }
+        }
+
+        return lastDay;
+    }
+
+    private boolean hasSize(int[] value, int length, int k) {
+
+        int currentSize = 0;
+
+        for (int i = 1; i <= length; i++) {
+
+            if (value[i] == 0) {
+                if (currentSize == k) {
+                    return true;
+                }
+
+                currentSize = 0;
+            } else {
+                currentSize++;
+            }
+        }
+
+        return currentSize == k;
+    }
+
+    public static void main(String[] args) {
+
+        LastDayWithBlockSize d = new LastDayWithBlockSize();
+
+        int[] ints = {3, 1, 5, 4, 2};
+        int k = 5;
+
+        System.out.println(d.lastDay(ints, k));
+    }
+}
---
+++ src/play/PlayTreeSet.java
@@ -0,0 +1,16 @@
+package play;
+
+import java.util.TreeSet;
+
+/**
+ * Created by tianle on 2/24/18.
+ */
+public class PlayTreeSet {
+
+    public static void main(String[] args) {
+
+        TreeSet<Object> objects = new TreeSet<>();
+
+        objects.ceiling(1);
+    }
+}

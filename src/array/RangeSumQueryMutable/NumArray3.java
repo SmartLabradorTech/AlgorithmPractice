94d1b8bb20069ba89cc402cb56e476f9581540be
Tianle Huang <tianlehuang@gmail.com>
2020-07-18 20:01:00 -0700

    range sum

---
+++ src/array/RangeSumQueryMutable/NumArray3.java
@@ -0,0 +1,38 @@
+package array.RangeSumQueryMutable;
+
+public class NumArray3 {
+
+    public NumArray3(int[] nums) {
+
+
+    }
+
+    public void update(int i, int val) {
+
+    }
+
+    public int sumRange(int i, int j) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        NumArray3 na = new NumArray3(null);
+
+        int a = 2;
+
+        System.out.println(Integer.toBinaryString(a));
+
+        System.out.println(Integer.toBinaryString(-1));
+
+    }
+}
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
